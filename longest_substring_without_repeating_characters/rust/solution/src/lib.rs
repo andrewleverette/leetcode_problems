14f5c7a040ab83e0e186b9e22a3ca3d0c29177f2
Andrew Leverette <andrewleverette@gmail.com>
2020-04-27 07:24:58 -0500

    Run cargo fmt and cargo clippy

--- longest_substring_without_repeating_characters/rust/solution/src/lib.rs
+++ longest_substring_without_repeating_characters/rust/solution/src/lib.rs
@@ -1,17 +1,17 @@
 use std::cmp;
 
 /// Returns the length of the longest substring without repeating characters.
-/// 
+///
 /// # Arguments
-/// 
+///
 /// * `s` - The input string
-/// 
+///
 /// # Approach
-/// 
-/// This solution uses a sliding window approach which keeps track of the 
+///
+/// This solution uses a sliding window approach which keeps track of the
 /// starting and ending positions of the current substring. For each iteration,
 /// the current character is checked against the characters seen so far.
-/// If that character has already been seen, then slide the starting position 
+/// If that character has already been seen, then slide the starting position
 /// forward to the ending position of the current substring. This solution works
 /// if you can make safe assumptions about the characters set. In this case I assume
 /// the character set is standard ASCII.
@@ -35,16 +35,25 @@ mod tests {
     use super::*;
     #[test]
     fn test_example_1() {
-        assert_eq!(length_longest_substring_sliding_window_optimized(String::from("abcabcbb")), 3);
+        assert_eq!(
+            length_longest_substring_sliding_window_optimized(String::from("abcabcbb")),
+            3
+        );
     }
 
     #[test]
     fn test_example_2() {
-        assert_eq!(length_longest_substring_sliding_window_optimized(String::from("bbbb")), 1);
+        assert_eq!(
+            length_longest_substring_sliding_window_optimized(String::from("bbbb")),
+            1
+        );
     }
 
     #[test]
     fn test_example_3() {
-        assert_eq!(length_longest_substring_sliding_window_optimized(String::from("pwwkew")), 3);
+        assert_eq!(
+            length_longest_substring_sliding_window_optimized(String::from("pwwkew")),
+            3
+        );
     }
 }
