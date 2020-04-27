c9da301a5a253ac5425042a77fe731634d832231
Andrew Leverette <andrewleverette@gmail.com>
2020-04-27 07:55:54 -0500

    Add function doc string

--- longest_substring_without_repeating_characters/python/solution.py
+++ longest_substring_without_repeating_characters/python/solution.py
@@ -1,4 +1,21 @@
 def length_of_longest_substring_sliding_window_optimized(s: str) -> int:
+    """
+    Returns the length of the longest substring without repeating characters.
+    
+    Arguments:
+    
+    * `s` - The input string
+    
+    Approach:
+    
+    This solution uses a sliding window approach which keeps track of the
+    starting and ending positions of the current substring. For each iteration,
+    the current character is checked against the characters seen so far.
+    If that character has already been seen, then slide the starting position
+    forward to the ending position of the current substring. This solution works
+    if you can make safe assumptions about the characters set. In this case I assume
+    the character set is standard ASCII.
+    """
     char_set = [0] * 128
     length = 0
 
