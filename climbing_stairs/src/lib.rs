2a7eb812a5e3cc2bba7e088a7b3010614ef30f5a
andrewleverette <andrew.leverette@decisionsource.com>
2020-07-31 10:52:23 -0500

    Add memoization solution to climbing stairs

--- climbing_stairs/src/lib.rs
+++ climbing_stairs/src/lib.rs
@@ -1,5 +1,20 @@
+use std::collections::HashMap;
+
 pub fn climb_stairs(n: i32) -> i32 {
-    unimplemented!();
+    let mut memoize: HashMap<i32, i32> = HashMap::new();
+    memoize.insert(1, 1);
+    memoize.insert(2, 2);
+    backtrack(n, &mut memoize)
+}
+
+fn backtrack(n: i32, memoize: &mut HashMap<i32, i32>) -> i32 {
+    if let Some(&steps) = memoize.get(&n) {
+        steps
+    } else {
+        let steps = backtrack(n - 1, memoize) + backtrack(n - 2, memoize);
+        memoize.insert(n, steps);
+        steps
+    }
 }
 
 #[cfg(test)]
