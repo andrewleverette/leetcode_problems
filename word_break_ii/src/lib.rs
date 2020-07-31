7e9932bea7aa980b258a45ddf96b89b7282f342a
Andrew Leverette <andrewleverette@gmail.com>
2020-07-31 06:23:55 -0500

    Add implementaion of word break II

--- word_break_ii/src/lib.rs
+++ word_break_ii/src/lib.rs
@@ -1,5 +1,70 @@
+use std::collections::HashMap;
+
+/// Finds sentences that can be created given a non-empty string and a
+/// dictionary as a list of words.
+/// 
+/// # Arguments
+/// 
+/// * `s` - The original string
+/// * `word_dict` - List of valid words 
 pub fn word_break(s: String, word_dict: Vec<String>) -> Vec<String> {
-    unimplemented!();
+    let mut sentence_map = HashMap::new();
+    sentence_builder(s, &word_dict, &mut sentence_map)
+}
+
+/// Recursively builds a list of sentences for each substring of 
+/// `s` and finally returns the list of sentences for the original 
+/// `s`
+/// 
+/// # Arguments
+/// 
+/// * `s` - A substring of the original string
+/// * `word_dict` - A reference to the list of valid words
+/// * `sentence_map` - A map of substrings to the current list of possible sentences
+fn sentence_builder(
+    s: String,
+    word_dict: &[String],
+    sentence_map: &mut HashMap<String, Vec<String>>,
+) -> Vec<String> {
+    // Base case return empty string if substring is empty
+    if s.is_empty() {
+        return vec![String::new()];
+    }
+
+    // Base case return current list of sentences if substring
+    // has been seen before
+    if let Some(words) = sentence_map.get(&s) {
+        return words.to_vec();
+    }
+
+    let mut sentences = Vec::new();
+
+    for word in word_dict {
+        // Check if the beginning of the substring
+        // matches a word in the word list, skip
+        // if it doesn't
+        match s.get(0..word.len()) {
+            Some(slice) => {
+                if slice != word {
+                    continue;
+                }
+            }
+            None => continue,
+        }
+
+        // Recurse on substring of any matches and update the current
+        // sentence list
+        let partials = sentence_builder(s[word.len()..].to_owned(), word_dict, sentence_map);
+        for partial in partials {
+            let space = if partial.is_empty() { "" } else { " " };
+            sentences.push(format!("{}{}{}", word, space, partial));
+        }
+    }
+
+    // Update current sentence list for substring
+    sentence_map.insert(s, sentences.to_owned());
+
+    sentences
 }
 
 #[cfg(test)]
@@ -17,7 +82,7 @@ mod tests {
             "dog".into(),
         ];
 
-        let expected: Vec<String> = vec!["cats and dog".into(), "cat sand dog".into()];
+        let expected: Vec<String> = vec!["cat sand dog".into(), "cats and dog".into()];
 
         assert_eq!(word_break(s, word_dict), expected);
     }
@@ -35,8 +100,8 @@ mod tests {
 
         let expected: Vec<String> = vec![
             "pine apple pen apple".into(),
-            "pineapple pen apple".into(),
             "pine applepen apple".into(),
+            "pineapple pen apple".into(),
         ];
 
         assert_eq!(word_break(s, word_dict), expected);
