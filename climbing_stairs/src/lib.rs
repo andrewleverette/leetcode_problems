7cbd01517538347f885ee542bd7a3ec0f4f818cc
Andrew Leverette <andrewleverette@gmail.com>
2020-07-31 06:32:53 -0500

    Start climbing stairs problem

---
+++ climbing_stairs/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn climb_stairs(n: i32) -> i32 {
+    unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example_1() {
+        assert_eq!(climb_stairs(2), 2);
+    }
+
+    #[test]
+    fn test_example_2() {
+        assert_eq!(climb_stairs(3), 3);
+    }
+
+}
