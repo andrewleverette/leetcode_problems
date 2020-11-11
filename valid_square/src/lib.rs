ac2625fbfb22b6376919007737b4fe67fe1625ed
Andrew Leverette <andrewleverette@gmail.com>
2020-11-11 11:37:42 -0600

    Update valid square

--- valid_square/src/lib.rs
+++ valid_square/src/lib.rs
@@ -1,31 +1,15 @@
 pub fn valid_square(p1: Vec<i32>, p2: Vec<i32>, p3: Vec<i32>, p4: Vec<i32>) -> bool {
-    let d2 = square_distance(&p1, &p2);
-    let d3 = square_distance(&p1, &p3);
-    let d4 = square_distance(&p1, &p4);
+    let mut points = vec![p1, p2, p3, p4];
+    points.sort_by(|a, b| {
+        match a[0] == b[0] {
+            true => a[1].cmp(&b[1]),
+            false => a[0].cmp(&b[0]),
+        }
+    });
 
-    if d2 == 0 || d3 == 0 || d4 == 0 {
-        false
-    } else if d2 == d3
-        && 2 * d2 == d4
-        && 2 * square_distance(&p2, &p4) == square_distance(&p2, &p3)
-        && square_distance(&p2, &p4) == square_distance(&p3, &p4)
-    {
-        true
-    } else if d2 == d4 
-        && 2 * d2 == d3 
-        && 2 * square_distance(&p2, &p3) == square_distance(&p2, &p4)
-        && square_distance(&p2, &p3) == square_distance(&p4, &p1)
-    {
-        true
-    } else if d3 == d4 
-        && 2 * d3 == d2 
-        && 2 * square_distance(&p3, &p2) == square_distance(&p3, &p4)
-        && square_distance(&p3, &p2) == square_distance(&p4, &p1)
-    {
-        true
-    } else {
-        false
-    }
+    square_distance(&points[0], &points[1]) != 0
+        && square_distance(&points[0], &points[1]) == square_distance(&points[0], &points[2])
+        && 2 * square_distance(&points[0], &points[1]) == square_distance(&points[0], &points[3]) 
 }
 
 fn square_distance(a: &Vec<i32>, b: &Vec<i32>) -> i32 {
